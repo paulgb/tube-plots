4efc8a563e798c6ff5323d3d2b6ec4f214308954
Paul Butler <paulgb@gmail.com>
2021-10-09 06:29:26 -0400

    update

--- code/20201219-perlin.jl
+++ code/20201219-perlin.jl
@@ -1,27 +1,59 @@
 ### A Pluto.jl notebook ###
-# v0.12.17
+# v0.14.0
 
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ b5c883c4-421e-11eb-01d5-279132f89203
-using PenPlots, Random, PlutoUI, Colors, Base.Iterators
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
 
-# ╔═╡ be833c48-421e-11eb-05c1-09da812c55c6
-noise = random_vector_matrix(MersenneTwister(9), 6, 8)
+# ╔═╡ 0502b791-fcf4-4cf7-9788-aa966b8e3307
+begin
+	import Pkg
+	Pkg.activate(mktempdir())
+	Pkg.add([
+			Pkg.PackageSpec(url="https://github.com/paulgb/PenPlots.jl"),
+			])
+	Pkg.add("PlutoUI")
+	using PenPlots
+	using Random
+	using Base.Iterators
+	using PlutoUI
+end
 
-# ╔═╡ c7e66c2e-421e-11eb-0a2e-29a57001cc86
-map(0:0.01:1) do j
-	r = 1 + perlin_noise(noise, Point(j*6, 0))
-	center = 3 * frac_rotation(j) * unitvec * r
+# ╔═╡ 7573514a-eb19-4fa6-9eb1-65e2de93e268
+md"
+Seed: $(@bind seed NumberField(1:100000))
 
-	Path(map(0:0.01:1) do i
-		r = 1 + perlin_noise(noise, Point(i*6, j*8))
-    	center + frac_rotation(i) * unitvec * r
-  end)
+Big Period: $(@bind big_period NumberField(1:100, default=7))
+
+Small Period: $(@bind small_period NumberField(1:100, default=4))
+
+Outer Radius: $(@bind radius NumberField(1:0.1:10, default=5))
+"
+
+# ╔═╡ 0bf0ad83-0632-4cdc-bb2f-f14dffc245cf
+begin
+	noise = random_vector_matrix(MersenneTwister(seed), big_period, small_period)
+	
+	map(0:0.004:1) do j
+		r = 1 + perlin_noise(noise, Point(j*big_period, 0))
+		center = radius * frac_rotation(j) * unitvec * r
+
+		Path(map(0:0.01:1) do i
+			r = 1 + perlin_noise(noise, Point(i*big_period, j*small_period))
+			center + frac_rotation(i) * unitvec * r
+	  end)
+	end
 end
 
 # ╔═╡ Cell order:
-# ╠═b5c883c4-421e-11eb-01d5-279132f89203
-# ╠═be833c48-421e-11eb-05c1-09da812c55c6
-# ╠═c7e66c2e-421e-11eb-0a2e-29a57001cc86
+# ╠═0502b791-fcf4-4cf7-9788-aa966b8e3307
+# ╠═7573514a-eb19-4fa6-9eb1-65e2de93e268
+# ╠═0bf0ad83-0632-4cdc-bb2f-f14dffc245cf
